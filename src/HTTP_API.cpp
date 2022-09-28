54736bf276ed92426a0f285d6c33392d50d8dcd4
Niklas Koll <nikkol567@gmail.com>
2022-09-28 08:42:53 +0200

    Small change #2

--- src/HTTP_API.cpp
+++ src/HTTP_API.cpp
@@ -100,8 +100,8 @@ void HTTP_API::save_new_effect() {
 
     String effectName = "";
 
-    auto *names = new String[10];
-    auto *times = new uint32_t[10];
+    auto names = new String[10];
+    auto times = new uint32_t[10];
 
     int clockIndex = 0;
     int argCount = server.args();
